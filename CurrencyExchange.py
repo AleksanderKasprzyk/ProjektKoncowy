0a9a5384637f00ffee5251560850bf573404654b
Aleksander Kasprzyk <apropolos@gmail.com>
2023-10-19 00:56:23 +0200

    Kod w polowie

--- CurrencyExchange.py
+++ CurrencyExchange.py
@@ -9,123 +9,29 @@ app.config["SECRET_KEY"] = "secret_key"
 api_flask = 'http://api.nbp.pl/'
 account_balance = float(0)
 currency_list = []
-
-
-# Kupno walut
-def buy_currency(currency_code, amount):
-    exchange_rate = nbp_data(f'exchangerates/rates/A/{currency_code}/?format=json')
-    if exchange_rate:
-        buy_rate = exchange_rate['rates'][0]['mid']
-        cost = buy_rate * amount
-        if cost > account_balance:
-            print("Niewystarczające saldo na koncie.")
-        else:
-            account_balance -= cost
-            print("Kupiono {} {} za {:.2f} PLN.".format(amount, currency_code, cost))
-    else:
-        print("Nie można uzyskać kursu wymiany.")
-
-
-# Sprzedaż walut
-def sell_currency(currency_code, amount):
-    exchange_rate = nbp_data(f'exchangerates/rates/A/{currency_code}/?format=json')
-    if exchange_rate:
-        sell_rate = exchange_rate['rates'][0]['mid']
-        sale_proceeds = sell_rate * amount
-        account_balance += sale_proceeds
-        print("Sprzedano {} {} za {:.2f} PLN.".format(amount, currency_code, sale_proceeds))
-    else:
-        print("Nie można uzyskać kursu wymiany.")
-
-
-# Portfel osobisty
-def personal_wallet():
-    print("Portfel osobisty:")
-    for currency in currencies[0]['rates']:
-        code = currency['code']
-        amount = 0  # Wprowadź ilość waluty w portfelu
-        print("{}: {:.2f}".format(code, amount))
-
-
-while True:
-    print("\nWybierz operację:")
-    print("1. Dodaj środki do konta")
-    print("2. Odbierz środki z konta")
-    print("3. Kup walutę")
-    print("4. Sprzedaj walutę")
-    print("5. Wyświetl saldo konta")
-    print("6. Wyjdź")
-
-    choice = input("Wybierz numer operacji: ")
-
-    if choice == '1':
-        amount = float(input("Podaj kwotę do dodania: "))
-        add_funds(amount)
-    elif choice == '2':
-        amount = float(input("Podaj kwotę do odebrania: "))
-        withdraw_funds(amount)
-    elif choice == '3':
-        currency_code = input("Podaj kod waluty do zakupu: ")
-        amount = float(input("Podaj ilość do zakupu: "))
-        buy_currency(currency_code, amount)
-    elif choice == '4':
-        currency_code = input("Podaj kod waluty do sprzedaży: ")
-        amount = float(input("Podaj ilość do sprzedaży: "))
-        sell_currency(currency_code, amount)
-    elif choice == '5':
-        print("Saldo konta: {:.2f} PLN".format(account_balance))
-    elif choice == '6':
-        break
-    else:
-        print("Niepoprawny wybór. Wybierz ponownie.")
-
-# Po zakończeniu działania programu możesz zapisać saldo konta do pliku
-save_to_file('saldo_konta.json', {"saldo": account_balance})
+operation_history = []
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
+    data, error_message = nbp_data('example_endpoint')  # Zaktualizuj endpoint
+
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
+        if data is not None:
+            if 'file' not in request.files:
+                return redirect(request.url)
 
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
+            file = request.files['file']
+            if file.filename == '':
+                return redirect(request.url)
 
-        now = datetime.now()
-        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+            now = datetime.now()
+            current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+            return render_template('Home_page.html', current_time=current_time)
 
-        return render_template('Home_page.html', current_time=current_time)
+        return render_template('Home_page.html', data=data, error_message=error_message)
 
-        # Obsługa operacji bankowych
-        operation = request.form.get('operation')
-        if operation == 'add_funds':
-            amount = float(request.form.get('amount'))
-            # Dodaj środki do konta
-            # Tutaj możesz użyć funkcji `add_funds`
-        elif operation == 'withdraw_funds':
-            amount = float(request.form.get('amount'))
-            # Odejmij środki z konta
-            # Tutaj możesz użyć funkcji `withdraw_funds`
-        elif operation == 'buy_currency':
-            currency_code = request.form.get('currency_code')
-            amount = float(request.form.get('amount'))
-            # Kup walutę
-            # Tutaj możesz użyć funkcji `buy_currency`
-        elif operation == 'sell_currency':
-            currency_code = request.form.get('currency_code')
-            amount = float(request.form.get('amount'))
-            # Sprzedaj walutę
-            # Tutaj możesz użyć funkcji `sell_currency`
-
-        # Pobierz saldo konta
-        # Tutaj możesz użyć funkcji do pobrania salda konta
-
-        # Pobierz listę walut
-    currencies = nbp_data('exchangerates/tables/A/?format=json')
-
-    return render_template('Home_page.html', account_balance=account_balance, currencies=currencies)
+    return render_template('Home_page.html', account_balance=account_balance, error_message=None)
 
 
 @app.route('/nbp_data', methods=['GET', 'POST'])
@@ -135,19 +41,27 @@ def nbp_data(endpoint):
         if response.status_code == 200:
             return response.json()
         else:
-            print("Error while downloading data.")
+            error_message = "Error while downloading data."
 
-        return render_template('nbp_data.html', response=response)
+        return render_template('nbp_data.html', response=response, error_message=error_message)
 
-    return redirect(request.url)
+    return redirect(url_for('/'))
 
 
 @app.route('/save_to_file', methods=['GET', 'POST'])
-def save_to_file(filename, data):
-    with open(filename, mode='w') as file:
-        json.dump(data, file, indent=4)
-
+def save_to_file():
     if request.method == 'POST':
+        data_save = \
+            {
+                "balance": account_balance,
+                "operation_history": operation_history
+            }
+
+        # with open(filename, mode='w') as file:
+        # json.dump(data, file, indent=4)
+
+        with open('operation_history.json', mode='w') as file:
+            json.dump(data_save, file, indent=4)
 
         currencies = nbp_data('exchangerates/tables/A/?format=json')
         if currencies:
@@ -160,22 +74,83 @@ def save_to_file(filename, data):
         return render_template('save_to_file.html', transaction_history=transaction_history,
                                currencies=currencies)
 
-    return redirect(request.url)
+    return redirect(url_for('/'))
 
 
-# Funkcja do dodawania środków do konta
 @app.route('/account_balance_operations.html', methods=['GET', 'POST'])
 def account(amount):
     global account_balance
     if request.method == 'POST':
-        account_balance += amount
-        return "Dodano {:.2f} PLN do konta.".format(amount)
+        operation_add = request.form.get('add')
+        operation_substract = request.form.get('substract')
 
-    if amount > account_balance:
-        return "Niewystarczające saldo na koncie."
-    else:
-        account_balance -= amount
-        return "Odebrano {:.2f} PLN z konta.".format(amount)
+        if operation_add == 'add':
+            amount = float(request.form['add_amount'])
+            account_balance += amount
+            operation_history.append(f"Added {amount}")
+            account('balance_account.json', {"balance": account_balance})
+            return render_template('/account_balance_operations.html', account=account, account_balance=account_balance,
+                                   amount=amount)
+
+        elif operation_substract == 'substract':
+            if amount > account_balance:
+                operation_history.append(f"Lack of sufficient resources to receive {amount}")
+            else:
+                amount = float(request.form['substract_amount'])
+                account_balance -= amount
+                operation_history.append(f"Substract {amount}")
+                account('balance_account.json', {"balance": account_balance})
+                return render_template('/account_balance_operations.html', account=account,
+                                       account_balance=account_balance, amount=amount)
+
+        return render_template('/account_balance_operations.html', operation_add=operation_add,
+                               operation_substract=operation_substract)
+
+    return redirect(url_for('/'))
+
+
+@app.route('/personal_wallet', methods=['GET', 'POST'])
+def bank_wallet():
+    if request.method == 'GET':
+        operation = request.form.get('operation')
+        if operation == 'buy_currency':
+            currency_code = request.form.get('currency_code')
+            exchange_rate = nbp_data(f'exchangerates/rates/A/{currency_code}/?format=json')
+            amount = float(request.form.get('amount'))
+
+            if exchange_rate:
+                buy_rate = exchange_rate['rates'][0]['mid']
+                cost = buy_rate * amount
+                if cost > account_balance:
+                    answer = "Insufficient balance in the account."
+                    return render_template('/personal_wallet', amount=amount, buy_rate=buy_rate, cost=cost,
+                                           answer=answer, exchange_rate=exchange_rate, currency_code=currency_code)
+                else:
+                    account_balance -= cost
+                    answer = "Bought {} {} for {:.2f} in PLN.".format(amount, currency_code, cost)
+                    return render_template('/personal_wallet', answer=answer)
+            else:
+                answer = "The exchange rate cannot be obtained."
+                return render_template('/personal_wallet', answer=answer)
+
+        elif operation == 'sell_currency':
+            currency_code = request.form.get('currency_code')
+            exchange_rate = nbp_data(f'exchangerates/rates/A/{currency_code}/?format=json')
+            amount = float(request.form.get('amount'))
+
+            if exchange_rate:
+                sell_rate = exchange_rate['rates'][0]['mid']
+                sale_proceeds = sell_rate * amount
+                account_balance += sale_proceeds
+                answer = "Sold {} {} for {:.2f} in PLN.".format(amount, currency_code, sale_proceeds)
+                return render_template('/personal_wallet', answer=answer, sell_rate=sell_rate,
+                                       sale_proceeds=sale_proceeds, exchange_rate=exchange_rate,
+                                       currency_code=currency_code)
+            else:
+                answer = "The exchange rate cannot be obtained."
+                return render_template('/personal_wallet', answer=answer)
+
+    return render_template('/personal_wallet', operation=operation)
 
 
 if __name__ == '__main__':
